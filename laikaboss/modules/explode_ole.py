46726f7891cbc2f84014b5e247afed2671ab4605
Frank Poz <knowmalware@users.noreply.github.com>
2016-09-22 01:07:42 +0000

    explode_ole: use scanObject.buffer directly instead of a copy
    Should be more efficient and quicker, especially with larger files.

--- laikaboss/modules/explode_ole.py
+++ laikaboss/modules/explode_ole.py
@@ -41,11 +41,7 @@ class EXPLODE_OLE(SI_MODULE):
                 raise
             except:
                 pass 
-        file = StringIO.StringIO()
-        file.write(scanObject.buffer)
-        file.flush()
-        file.seek(0)
-        ole = olefile.OleFileIO(file)
+        ole = olefile.OleFileIO(scanObject.buffer)
         
         lstStreams = ole.listdir()
         numStreams = 0
@@ -72,5 +68,4 @@ class EXPLODE_OLE(SI_MODULE):
             except:
                 log_module("MSG", self.module_name, 0, scanObject, result, "ERROR EXTRACTING STREAM: "+str(stream))
         ole.close()
-        file.close()
         return moduleResult
